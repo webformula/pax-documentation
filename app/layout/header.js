2746433047653e101aa453be4161424d3ca1bd0a
Ben Rubin <ben@benjaminrubin.com>
2019-02-19 09:34:04 -0600

    update version to be a link update header text

--- app/layout/header.js
+++ app/layout/header.js
@@ -4,8 +4,8 @@ const { dependencies } = require('../../package.json');
 module.exports = ({ title }) => html`
   <div style="height: 56px;">
     <header>
-      <h2>web-componenets-node</h2>
-      <span class="version">${dependencies['@webformula/pax-core'].replace('^' , '')}</span>
+      <h2>PAX</h2>
+      <span class="version"><a href="https://github.com/webformula/pax-core/releases/tag/v${dependencies['@webformula/pax-core'].replace('^' , '')}" target="_blank">${dependencies['@webformula/pax-core'].replace('^' , '')}</a></span>
       <span class="subtext">Welcome to the wounderful world of the web</span>
     </header>
   </div>
