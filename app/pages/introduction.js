3741d33b03e0b222ccdac89746e4a379f21926b7
Ben Rubin <ben@benjaminrubin.com>
2019-02-19 10:07:33 -0600

    fix text

--- app/pages/introduction.js
+++ app/pages/introduction.js
@@ -31,7 +31,7 @@ module.exports = class Introduction extends Page {
           <p>This project promotes the usage of web components. If you are not familiar with the web-component specs then I would suggest reading them first. <a href="https://developer.mozilla.org/en-US/docs/Web/Web_Components">Link</a></p>
 
           <h5>What</h5>
-          <p><b>P.A.C.T.S. (pages and components tool set)</b> is a set of tools to allow web pages to be built simply, nativly and without stacks of dependencies. The tools add some great features including server-side rendering, dynamic client side rendering, performance (using native browser features), optimizations that reduces load times, managed client side caching, pwa compatability, and more.</p>
+          <p><b>PAX</b> is a set of tools to allow web pages to be built simply, nativly and without stacks of dependencies. The tools add some great features including server-side rendering, dynamic client side rendering, performance (using native browser features), optimizations that reduces load times, managed client side caching, pwa compatability, and more.</p>
 
           <h5>Why</h5>
           <p>Browsers, javascript, css, and html are evolving in a uniform and predictable pace. Many features that are provided by frameworks(react, angular, vue, jquery) are now available in native forms in the browser. It is time to start looking at building web sites and applications the way we used to, nativly and with out loads of dependencies. With web-components spec becoming V1 we now have one of the last major features that frameworks provide in a native form (custom html with code associated to it). In short lets learn / return to javascript, css, and html at its core</p>
