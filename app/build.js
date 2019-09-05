1a4f85e22801fa3ecfa90cf187ae9c0538fc7b30
Ben Rubin <ben@benjaminrubin.com>
2019-09-05 15:32:00 -0500

    up

--- app/build.js
+++ app/build.js
@@ -6,7 +6,7 @@ build({
   layoutFilePath: 'app/layout/index.js',
   distFolder: 'dist',
   routeConfig: {
-    root: 'Introduction',
-    fourOFour: 'FourOFour'
+    root: 'introduction',
+    fourOFour: 'fourOFour'
   }
 });
